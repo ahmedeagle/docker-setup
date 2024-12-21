b9f4c87d0f4fc88d354e77bfe072953608e2c73b
Ahmed Emam <aemam@trudochealth.com>
2024-12-21 23:58:46 +0400

    docker setup for laravel dev envirnoment

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,4 +1,15 @@
-SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = 'your_db_username') AS user_exists;
-CREATE USER IF NOT EXISTS 'your_db_username'@'%';
-GRANT ALL PRIVILEGES ON emr2.* TO 'your_db_username'@'%';
+-- Use the MYSQL_USER and MYSQL_PASSWORD environment variables
+SET @user = '${MYSQL_USER}';
+SET @password = '${MYSQL_PASSWORD}';
+
+-- Check if the user exists
+SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = @user) AS user_exists;
+
+-- Create the user if it doesn't exist
+CREATE USER IF NOT EXISTS @user@'%' IDENTIFIED BY @password;
+
+-- Grant privileges
+GRANT ALL PRIVILEGES ON `${MYSQL_DATABASE}`.* TO @user@'%';
+
+-- Apply the changes
 FLUSH PRIVILEGES;
